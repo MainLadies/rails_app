8b521d7ec61fd1046b9905b328f273bea1bfaec2
Miona <lgaowei1@gmail.com>
2022-04-03 10:00:16 +0900

    Initial Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,62 @@
+class PostsController < ApplicationController
+
+    before_action :authenticate_user!
+
+    def index
+        @posts = Post.all
+
+        @comment = Comment.new
+
+        if params[:search] == nil
+            @posts= Post.all
+        elsif params[:search] == ''
+            @posts= Post.all
+        else
+        #部分検索
+            @posts = Post.where("post_name LIKE ? ",'%' + params[:search] + '%')
+        end
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        post = Post.new(post_params)
+        post.user_id = current_user.id
+        if post.save
+            redirect_to :action => "index"
+        else
+            redirect_to :action => "new"
+        end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        post = Post.find(params[:id])
+        if post.update(post_params)
+        redirect_to :action => "show", :id => post.id
+        else
+            redirect_to :action => "new"
+        end
+    end
+
+    def destroy
+        post = Post.find(params[:id])
+        post.destroy
+        redirect_to action: :index
+    end
+
+    private
+    def post_params
+    params.require(:post).permit(:post_name, :image)
+    end
+    
+end
